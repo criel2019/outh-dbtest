4794891335dedfffc69b2d2dc4918ccaed6009c0
criel2019 <pjhk579700@gmail.com>
2021-04-13 23:09:23 +0900

    Update user.ts

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -105,7 +105,7 @@ module.exports = function (app) {
         if (!doc) {
           const newUser = new User({
             naverId: profile.id,
-            username: profile.name,
+            // username: profile.name,
             email: profile.emails[0].value,
             thumbnail: profile.photos[0].value
           });
