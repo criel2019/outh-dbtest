c6622884c78b5bdf7540df8b8d12ffa6ad70b327
criel2019 <pjhk579700@gmail.com>
2021-04-13 22:43:26 +0900

    Update user.ts

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,9 +2,9 @@ export interface IMongoDBUser {
     googleId?: string;
     kakaoId?: string;
     naverId?: string;
-    username: string;
-    email: string;
-    thumbnail: string;
-    __v: number;
-    _id: string;
+    username?: string;
+    email?: string;
+    thumbnail?: string;
+    __v?: number;
+    _id?: string;
 }
