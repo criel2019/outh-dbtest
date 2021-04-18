7fe522bff84caa54972ca90acfc6ebfa742f8c00
criel2019 <pjhk579700@gmail.com>
2021-04-18 18:14:54 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,24 @@ mongoDBStore.on("error", () => {
 
 
 app.use(cors({ origin: "https://criel-front.netlify.app", credentials: true }))
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://criel-front.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(
   session({ 
