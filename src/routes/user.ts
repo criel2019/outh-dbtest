e8d00f73723ee6f73d4024cf9f1274fc9e8717a5
criel2019 <pjhk579700@gmail.com>
2021-04-19 13:07:59 +0900

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -34,7 +34,7 @@ module.exports = function (passport) {
   router.get("/auth/logout", (req, res) => {
     req.session.destroy(() => {
       res.clearCookie('connect.sid');
-      res.redirect('https://criel-front.netlify.app');
+      res.redirect('/');
   });
   })
 
